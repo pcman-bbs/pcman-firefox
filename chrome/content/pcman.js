3c7e50d418055ea281f22d7db47f2b133a161b60
pcman.tw <pcman.tw@5d5222ee-d86d-11de-a9c8-216ecde6309e>
2009-11-28 20:27:29 +0000

    * Try to support 'Paste' (in popup menu). * Add very primitive popup menu.

--- chrome/content/pcman.js
+++ chrome/content/pcman.js
@@ -37,5 +37,39 @@ PCMan.prototype={
 
     onClose: function(conn) {
         alert(this.stringBundle.getString("alert_conn_close"));
+    },
+    
+    copy: function(){
+        alert('Not yet supported');
+    },
+
+    paste: function() {
+        if(this.conn) {
+            // From: https://developer.mozilla.org/en/Using_the_Clipboard
+            var clip = Components.classes["@mozilla.org/widget/clipboard;1"]
+                            .getService(Components.interfaces.nsIClipboard);
+            if(!clip)
+                return false;
+            var trans = Components.classes["@mozilla.org/widget/transferable;1"]
+                            .createInstance(Components.interfaces.nsITransferable);
+            if (!trans)
+                return false;
+            trans.addDataFlavor("text/unicode");
+            clip.getData(trans, clip.kGlobalClipboard);
+            var data={};
+            var len={};
+            trans.getTransferData("text/unicode", data, len);
+            if(data && data.value) {
+                var s=data.value.QueryInterface(Components.interfaces.nsISupportsString);
+                s = s.data.substring(0, len.value / 2);  
+                s=s.replace(/\r\n/g, '\r');
+                s=s.replace(/\n/g, '\r');
+                this.conn.convSend(s, 'big5');
+            }
+        }
+    },
+
+    selAll: function() {
+        alert('Not yet supported');
     }
 }
