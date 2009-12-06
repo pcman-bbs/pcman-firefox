71e592bdf99459db07f4c23ee4b377f7b1f0f20a
bootleq <bootleq@5d5222ee-d86d-11de-a9c8-216ecde6309e>
2009-12-06 02:52:59 +0000

    * Favicon & tab icons. (GPL license?) * Adjust code intends.

--- chrome/content/pcman.js
+++ chrome/content/pcman.js
@@ -28,6 +28,7 @@ PCMan.prototype={
     },
 
     onConnect: function(conn) {
+        this.updateTabIcon('connect');
     },
 
     onData: function(conn, data) {
@@ -38,6 +39,7 @@ PCMan.prototype={
 
     onClose: function(conn) {
         alert(this.stringBundle.getString("alert_conn_close"));
+        this.updateTabIcon('disconnect');
     },
     
     copy: function(){
@@ -81,92 +83,113 @@ PCMan.prototype={
         var relX = event.pageX - this.view.canvas.offsetLeft;
         var relY = event.pageY - this.view.canvas.offsetTop;
         var PosX = (relX - relX % this.view.chw) / this.view.chw;//too slow?
-	var PosY = (relY - relY % this.view.chh) / this.view.chh;
-	var uris = this.buf.lines[PosY].uris;
-	if (!uris) return;
-	for (var i=0;i<uris.length;i++){
-	  if (PosX >= uris[i][0] && PosX < uris[i][1]){ //@ < or <<
-	    var uri = "";
-	    for (var j=uris[i][0];j<uris[i][1];j++)
-	      uri = uri + this.buf.lines[PosY][j].ch;
-	    var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-                   .getService(Components.interfaces.nsIWindowMediator);
-	    var gBrowser = wm.getMostRecentWindow("navigator:browser").gBrowser;
-	    // gBrowser.selectedTab = gBrowser.addTab(uri);
-	    gBrowser.addTab(uri, gBrowser.currentURI);
-	  }
-	}
-  },
+        var PosY = (relY - relY % this.view.chh) / this.view.chh;
+        var uris = this.buf.lines[PosY].uris;
+        if (!uris) return;
+        for (var i=0;i<uris.length;i++) {
+          if (PosX >= uris[i][0] && PosX < uris[i][1]) { //@ < or <<
+            var uri = "";
+            for (var j=uris[i][0];j<uris[i][1];j++)
+              uri = uri + this.buf.lines[PosY][j].ch;
+            var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+            var gBrowser = wm.getMostRecentWindow("navigator:browser").gBrowser;
+            // gBrowser.selectedTab = gBrowser.addTab(uri);
+            gBrowser.addTab(uri, gBrowser.currentURI);
+          }
+        }
+    },
     //detect current location and change mouse cursor  
     mousemove: function(event) {
         var relX = event.pageX - this.view.canvas.offsetLeft;
         var relY = event.pageY - this.view.canvas.offsetTop;
         var PosX = (relX - relX % this.view.chw) / this.view.chw;//too slow?
-	var PosY = (relY - relY % this.view.chh) / this.view.chh;
-	if(PosY >= 24) return;   // ignore events out of "rows" range, it's possible since we don't resize canvas height to fit chh*24
-	var uris = this.buf.lines[PosY].uris;
-	if (!uris) {
-	  this.view.canvas.style.cursor = "default";
-	  return;
-	}
-	for (var i=0;i<uris.length;i++){
-	  if (PosX >= uris[i][0] && PosX < uris[i][1]){ //@ < or <<
-	    this.view.canvas.style.cursor = "pointer";
-	    return
-	  }
-	}
-	this.view.canvas.style.cursor = "default";
-  },
+        var PosY = (relY - relY % this.view.chh) / this.view.chh;
+        if(PosY >= 24) return;   // ignore events out of "rows" range, it's possible since we don't resize canvas height to fit chh*24
+        var uris = this.buf.lines[PosY].uris;
+        if (!uris) {
+          this.view.canvas.style.cursor = "default";
+          return;
+        }
+        for (var i=0;i<uris.length;i++) {
+          if (PosX >= uris[i][0] && PosX < uris[i][1]) { //@ < or <<
+            this.view.canvas.style.cursor = "pointer";
+            return
+          }
+        }
+        this.view.canvas.style.cursor = "default";
+    },
   
-  textboxControllers: {
-    supportsCommand: function(cmd){
-      switch (cmd) {
-        case "cmd_undo":
-        case "cmd_redo":
-        case "cmd_cut":
-        case "cmd_copy":
-        case "cmd_paste":
-        case "cmd_selectAll":
-        case "cmd_delete":
-        case "cmd_switchTextDirection":
-        case "cmd_find":
-        case "cmd_findAgain":
-          return true;
-      }
+    textboxControllers: {
+      supportsCommand: function(cmd){
+        switch (cmd) {
+          case "cmd_undo":
+          case "cmd_redo":
+          case "cmd_cut":
+          case "cmd_copy":
+          case "cmd_paste":
+          case "cmd_selectAll":
+          case "cmd_delete":
+          case "cmd_switchTextDirection":
+          case "cmd_find":
+          case "cmd_findAgain":
+            return true;
+        }
+      },
+      isCommandEnabled: function(cmd){
+        switch (cmd) {
+          case "cmd_copy":
+          case "cmd_paste":
+          case "cmd_selectAll":
+            return true;
+          default:
+            return false;
+        }
+      },
+      doCommand: function(cmd){
+        switch (cmd) {
+          case "cmd_undo":
+          case "cmd_redo":
+          case "cmd_cut":
+            return true;
+          case "cmd_copy":
+            pcman.copy();
+            break;
+          case "cmd_paste":
+            pcman.paste();
+            break;
+          case "cmd_selectAll":
+            pcman.selAll();
+            break;
+          case "cmd_delete":
+          case "cmd_switchTextDirection":
+          case "cmd_find":
+          case "cmd_findAgain":
+            return true;
+        }
+      },
+      onEvent: function(e){ }
     },
-    isCommandEnabled: function(cmd){
-      switch (cmd) {
-        case "cmd_copy":
-        case "cmd_paste":
-        case "cmd_selectAll":
-          return true;
+
+    updateTabIcon: function(aStatus) {
+      var icon = 'chrome://pcmanfx2/skin/tab-connecting.png';
+      switch (aStatus) {
+        case 'connect':
+          icon =  'chrome://pcmanfx2/skin/tab-connect.png';
+          break;
+        case 'disconnect':
+          icon =  'chrome://pcmanfx2/skin/tab-disconnect.png';
+          break;
+        case 'idle':  // Not used yet
+          icon =  'chrome://pcmanfx2/skin/tab-idle.png';
+          break;
+        case 'connecting':  // Not used yet
         default:
-          return false;
       }
-    },
-    doCommand: function(cmd){
-      switch (cmd) {
-        case "cmd_undo":
-        case "cmd_redo":
-        case "cmd_cut":
-          return true;
-        case "cmd_copy":
-          pcman.copy();
-          break;
-        case "cmd_paste":
-          pcman.paste();
-          break;
-        case "cmd_selectAll":
-          pcman.selAll();
-          break;
-        case "cmd_delete":
-        case "cmd_switchTextDirection":
-        case "cmd_find":
-        case "cmd_findAgain":
-          return true;
+      var rw = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+      var browserIndex = rw.gBrowser.getBrowserIndexForDocument(document);
+      if (browserIndex > -1) {
+        rw.gBrowser.mTabContainer.childNodes[browserIndex].image = icon;
       }
-    },
-    onEvent: function(e){ }
-  }
+    }
 
 }
