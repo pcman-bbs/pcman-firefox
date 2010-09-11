1ca4e59ee7a4ef0214f3d16b277faf52888cfe62
u881831@hotmail.com <u881831@hotmail.com@5d5222ee-d86d-11de-a9c8-216ecde6309e>
2010-09-11 18:41:30 +0000

    Add the module of preference system (options doesn't affect the behaviour of the main program now)

---
+++ chrome/content/options.js
@@ -0,0 +1,101 @@
+function PCmanOptions() {
+    this.confFile = Components.classes["@mozilla.org/file/directory_service;1"]
+                             .getService(Components.interfaces.nsIProperties)
+                             .get("ProfD", Components.interfaces.nsIFile);
+    this.confFile.append('pcmanfx.ini');
+    if( !this.confFile.exists() )
+        this.confFile.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0666);
+
+    this.prefs = new IniFile();
+    this.prefs.load(this.confFile);
+    if( !this.prefs.groups['default'] ) {
+        this.reset('default');
+        this.prefs.save(this.confFile);
+    }
+    this.getBookmarkIDs();
+}
+
+PCmanOptions.prototype = {
+    getBookmarkIDs: function() {
+        var names = this.prefs.getGroupNames();
+        this.bookmarkIDs = [];
+        for(var i=0; i<names.length; ++i) {
+            if(names[i] != 'default') {
+                var id = parseInt(names[i].substr(6));
+                this.bookmarkIDs.push(id);
+            }
+        }
+    },
+
+    getGroupName: function(url) {
+        var browserutils = new BrowserUtils();
+        var bookmarkID = browserutils.findBookmarkID(url);
+        var group = bookmarkID ? ('rdf:#$' + bookmarkID) : 'default';
+        if( !this.prefs.groups[group] ) // Not created, use default
+            group = 'default';
+        return group;
+    },
+
+    create: function(url) {
+        var browserutils = new BrowserUtils();
+        var bookmarkID = browserutils.findBookmarkID(url);
+        if(bookmarkID) {
+            var group = 'rdf:#$' + bookmarkID;
+            this.reset(group);
+            this.prefs.save(this.confFile);
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    init: function() {
+        var browserutils = new BrowserUtils();
+        var bookmarkService = browserutils._bookmarkService;
+        var validIDs = [];
+        for(var i=0; i<this.bookmarkIDs.length; ++i) {
+            try {
+                var bookmarkTitle = bookmarkService.getItemTitle(this.bookmarkIDs[i]);
+                document.getElementById('siteList').appendItem(bookmarkTitle);
+                validIDs.push(this.bookmarkIDs[i]);
+            } catch (e) { // the bookmark may be removed
+                delete this.prefs.groups['rdf:#$' + this.bookmarkIDs[i]];
+            }
+        }
+        this.bookmarkIDs = validIDs;
+        this.recentGroup = 'default';
+        this.load(this.recentGroup);
+    },
+
+    siteChanged: function() {
+        this.save(this.recentGroup);
+        var siteList = document.getElementById('siteList');
+        var siteIndex = siteList.getIndexOfItem(siteList.selectedItems[0]);
+        if(siteIndex == 0)
+            var groupname = 'default';
+        else
+            var groupname = 'rdf:#$' + this.bookmarkIDs[siteIndex-1];
+        this.recentGroup = groupname;
+        this.load(this.recentGroup);
+    },
+
+    accept: function() {
+        this.save(this.recentGroup);
+        this.prefs.save(this.confFile);
+
+        // TODO: create an event to notify the main program that prefs were changed
+    },
+
+    load: function(group) {
+        document.getElementById('Charset').value = this.prefs.getStr(group, 'Encoding', 'big5');
+    },
+
+    save: function(group) {
+        this.prefs.setStr(group, 'Encoding', document.getElementById('Charset').value);
+    },
+
+    reset: function(group) {
+        this.prefs.setStr(group, 'Encoding', 'big5');
+    }
+}
+
