f5a7a08dc3ab41eee6f3bf3a45dc55a126d6335f
u881831@hotmail.com <u881831@hotmail.com@5d5222ee-d86d-11de-a9c8-216ecde6309e>
2012-02-08 13:33:29 +0000

    Fix the bug of clicking hyper-link

--- termview.js
+++ termview.js
@@ -800,7 +800,7 @@ TermView.prototype={
             else if(event.button == 1) // middle button
                 openURI(uri, false);
             else if(event.button == 2) // right button
-                //FIXME: add "Save As", "Copy Link" and so on to context menu
+                {} //FIXME: add "Save As", "Copy Link" and so on to context menu
             return;
         }
 
