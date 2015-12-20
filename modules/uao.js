412646b93dff0a36b510a3655e2b52fe0f4a2de8
u881831 <u881831@hotmail.com>
2015-12-20 13:06:12 +0800

    Fix UAO converter in FX 43+

--- modules/uao.js
+++ modules/uao.js
@@ -6,25 +6,26 @@ var EXPORTED_SYMBOLS = ["uaoConv"];
 
 var uaoConv = {
     u2bTab: '',
-    init: function () {
+    b2uTab: '',
+    init: function (type) {
         // load U2B table
         var ioService = Components.classes["@mozilla.org/network/io-service;1"]
                             .getService(Components.interfaces.nsIIOService);
         // load from resource:// instead of file path
-        var channel = ioService.newChannel('resource://pcmanfx2/u2b.tab', null, null);
+        var channel = ioService.newChannel('resource://pcmanfx2/'+type+'.tab', null, null);
         var ins = channel.open();
         var bins = Components.classes["@mozilla.org/binaryinputstream;1"].
                        createInstance(Components.interfaces.nsIBinaryInputStream);
         bins.setInputStream(ins);
         while(bins.available())
-            this.u2bTab += bins.readBytes(bins.available());
+            this[type+'Tab'] += bins.readBytes(bins.available());
         bins.close();
     },
 
     u2b: function(ustr) {
         var ret = '';
         if(!this.u2bTab)
-            this.init(); // initialize UAO table
+            this.init('u2b'); // initialize UAO table
         var u2b = this.u2bTab; // the table
         var i, n = ustr.length;
         for(i = 0; i < n; ++i) {
@@ -36,6 +37,31 @@ var uaoConv = {
                 if(idx < u2b.length) {
                     var big5 = u2b[idx] + u2b[idx + 1];
                     ret += big5;
+                }
+            }
+            else // this is an ascii character
+                ret += ch;
+        }
+        return ret;
+    },
+
+    b2u: function(data) {
+        var ret = '';
+        if(!this.b2uTab)
+            this.init('b2u'); // initialize UAO table
+        var b2u = this.b2uTab; // the table
+        var i, n = data.length;
+        for(i = 0; i < n; ++i) {
+            var ch = data[i];
+            var code = ch.charCodeAt(0);
+            if(code >= 129 && i < n-1) { // use UAO table
+                code = code * 256 + data.charCodeAt(i+1);
+                var idx = (code - 0x8001) * 2;
+                // dump('idx = ' + idx + ', len = ' + b2u.length + '\n');
+                if(idx < b2u.length) {
+                    var uni = b2u.charCodeAt(idx) * 256 + b2u.charCodeAt(idx+1);
+                    ret += String.fromCharCode(uni);
+                    ++i;
                 }
             }
             else // this is an ascii character
